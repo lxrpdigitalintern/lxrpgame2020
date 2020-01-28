6f03d65b8bdba9269f189f8302f0cf6a74a5054f
LXRP <Amshu.Amshumaan@spalliance.com.au>
2020-01-28 12:11:00 +1100

    First Commit

---
+++ LXRP_Builds/Assets/2_Scripts/Player Scripts/Mission Scripts/SO_QuestionInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Question 1", menuName = "ScriptableObjects/QuestionInfo", order = 2)]
+public class SO_QuestionInfo : ScriptableObject
+{
+    public int questionNo;
+    public string questionText;
+    public bool isCorrect;
+    public char answer;
+    public string correctText;
+    public string incorrectText;
+
+    private bool isSelected = false;
+    public bool IsSelected { get => isSelected; set => isSelected = value; }
+}
+
